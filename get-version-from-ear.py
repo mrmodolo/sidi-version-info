891cc1fad5f8ddb97e628953de699ffb41dac0ec
modolo <marcelo.rmodolo@gmail.com>
2016-11-20 18:22:29 -0200

    Quase no final!

--- get-version-from-ear.py
+++ get-version-from-ear.py
@@ -3,6 +3,8 @@
 
 import zipfile
 import sys
+import StringIO
+import re
 
 
 def argparse():
@@ -13,24 +15,36 @@ def argparse():
 def get_ear_filename():
     return sys.argv[1]
 
-def temp():
-    zf = zipfile.ZipFile('sidi.ear', 'r')
-    for filename in zf.namelist():
-        if 'war' in filename:
-            try:
-                data = zf.read(filename)
-                print type(data)
-                if 'versao' in data:
-                    print 'versao'
-            except KeyError:
-                print 'ERROR: Did not find %s in zip file' % filename
-            else:
-                print 'OK'
-            print
+def get_version_content_from_ear(ear_filename):
+    ear = get_ear_filename()
+    try:
+        with zipfile.ZipFile(ear, 'r') as ear_zf:
+            filename = [f for f in ear_zf.namelist() if 'war' in f]
+            if filename:
+                memory_war = StringIO.StringIO()
+                memory_war.write(ear_zf.open(filename[0]).read())
+                with zipfile.ZipFile(memory_war) as war_zf:
+                    filename = [f for f in war_zf.namelist() if 'versao.xml' in f]
+                    if filename:
+                        return war_zf.read(filename[0])
+    except IOError:
+        print 'Não foi possível abrir o arquivo %s' % ear
+        sys.exit(1)
+    except KeyError:
+        print 'Não foi possível encontrar o arquivo  %s no pacote' % filename
+        sys.exit(1)
+
+def get_version(content):
+    v = re.search('([0-9]*\.[0-9]*\.[0-9]*)',content)
+    return v.group(0)
 
 def main():
     argparse()
-    print get_ear_filename()
+    ear_filename = get_ear_filename()
+    versao_xml = get_version_content_from_ear(ear_filename)
+    # var version = '1.6.4';
+    print get_version(versao_xml)
+    sys.exit(0)
 
 if __name__ == "__main__":
         main()
